2ca5b1a947f7d587fa68eb5b990ec12fbc8d6f2b
Daniel Hurtado <a01707774@tec.mx>
2025-05-21 22:07:11 -0600

    fix: Add missing CIRCLE token to words dictionary in Lexer

--- Lexer.py
+++ Lexer.py
@@ -128,8 +128,7 @@ class Lexer:
     self.words["OR"] = Token(Tag.OR, "OR")
     self.words["AND"] = Token(Tag.AND, "AND")
     self.words["MOD"] = Token(Tag.MOD, "MOD")
-    self.words["CIRCLE"] = Token(Tag.CIRCLE, "CIRCLE") #Faltaba acá el circle
-
+    self.words["CIRCLE"] = Token(Tag.CIRCLE, "CIRCLE")
 
   def get_next_character(self):
     if len(self.current_buffer) == 0 and len(self.next_buffer) > 0:
